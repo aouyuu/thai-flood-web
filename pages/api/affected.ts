b50119248fe9cb9a91b585be4463c55af517db93
Manneaber <Manneaber@users.noreply.github.com>
2021-10-05 18:03:25 +0700

    Squashed commit of the following:
    commit efc5f4a07ac45832082bbcc0df4a096e81bb85ed
    Author: Manneaber <Manneaber@users.noreply.github.com>
    Date:   Tue Oct 5 17:33:31 2021 +0700
    
        Add mock server

---
+++ pages/api/affected.ts
@@ -0,0 +1,82 @@
+export type OverviewResponse = {
+  updateTimestamp: number;
+  date: number;
+  affectedAreas: {
+    id: number;
+    name: string;
+    affected: number;
+  }[];
+};
+
+export type DetailByProvinceResponse = {
+  updateTimestamp: number;
+  date: number;
+  id: number;
+  name: string;
+  district: {
+    id: number;
+    updateTimestamp: number;
+    name: string;
+    subdistrict: {
+      id: number;
+      updateTimestamp: number;
+      name: string;
+      affected: number;
+    }[];
+  }[];
+};
+
+export const getOverview = (): OverviewResponse => {
+  return {
+    updateTimestamp: 1633371269,
+    date: 1633284000,
+    affectedAreas: [
+      {
+        id: 1,
+        name: 'กรุงเทพ',
+        affected: 50,
+      },
+      {
+        id: 2,
+        name: 'ขอนแก่น',
+        affected: 100,
+      },
+    ],
+  };
+};
+
+export const getDetailByProvince = (): DetailByProvinceResponse => {
+  return {
+    updateTimestamp: 1633371269,
+    date: 1633371269,
+    id: 2,
+    name: 'ขอนแก่น',
+    district: [
+      {
+        id: 1,
+        updateTimestamp: 1633371269,
+        name: 'เมืองขอนแก่น',
+        subdistrict: [
+          {
+            id: 1,
+            updateTimestamp: 1633371269,
+            name: 'ในเมือง',
+            affected: 100,
+          },
+          {
+            id: 2,
+            updateTimestamp: 1633371269,
+            name: 'เมืองเก่า',
+            affected: 50,
+          },
+          {
+            id: 3,
+            updateTimestamp: 1633371269,
+            name: 'พระยืน',
+            affected: 0,
+          },
+        ],
+      },
+    ],
+  };
+};
